9f14b9e113fc7a1c87a54201c3ca350bf8a27bbd
after1990s <after1990s@gmail.com>
2013-03-19 12:14:10 +0800

    init
    intial

---
+++ SQL_Injector.h
@@ -0,0 +1,32 @@
+
+// SQL_Injector.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CSQL_InjectorApp:
+// 有关此类的实现，请参阅 SQL_Injector.cpp
+//
+
+class CSQL_InjectorApp : public CWinApp
+{
+public:
+	CSQL_InjectorApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSQL_InjectorApp theApp;
---
+++ SQL_InjectorDlg.h
@@ -0,0 +1,32 @@
+
+// SQL_InjectorDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CSQL_InjectorDlg 对话框
+class CSQL_InjectorDlg : public CDialogEx
+{
+// 构造
+public:
+	CSQL_InjectorDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_SQL_INJECTOR_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
